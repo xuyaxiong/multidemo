7e16b71117cf8636240c4768f032a2cc32dbdaa2
xuyaxiong <xyxlindy@163.com>
2022-05-10 11:54:57 +0800

    first commit

---
+++ src/main/java/com/example/multidemo/MultiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.multidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/multidemo/MultiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.multidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
