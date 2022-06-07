0c4bcaf581bda17bcfe048c8ccfe4187dbe5bef1
xuyaxiong <xyxlindy@163.com>
2022-06-07 19:24:31 +0800

    提交

--- src/test/java/com/example/multidemo/DocxTemplateTest.java
+++ src/test/java/com/example/multidemo/DocxTemplateTest.java
@@ -3,10 +3,14 @@ package com.example.multidemo;
 import com.deepoove.poi.XWPFTemplate;
 import com.deepoove.poi.config.Configure;
 import com.deepoove.poi.config.ConfigureBuilder;
+import com.deepoove.poi.data.Includes;
+import com.example.multidemo.model.AddrModel;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class DocxTemplateTest {
     @Test
@@ -18,6 +22,10 @@ public class DocxTemplateTest {
             put("title", "这是标题");
             put("content", "这是正文");
             put("sex", true);
+            List<AddrModel> subData = new ArrayList<>();
+            subData.add(new AddrModel("Hangzhou,China"));
+            subData.add(new AddrModel("Shanghai,China"));
+            put("nested", Includes.ofLocal("sub_template.docx").setRenderModel(subData).create());
         }}).writeToFile("out_template.docx");
     }
 }
---
+++ src/test/java/com/example/multidemo/model/AddrModel.java
@@ -0,0 +1,10 @@
+package com.example.multidemo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AddrModel {
+    private String addr;
+}
